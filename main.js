655ca8408a4745b3aec1d54039e5bd358bcb224d
Aksa-Rose <aksarose3@gmail.com>
2024-07-17 23:52:19 +0530

    build: gemini fully wrking with the library

--- main.js
+++ main.js
@@ -1,42 +1,12 @@
-/*const {Client} = require ('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');*/
+const {Client} = require ('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 require('dotenv').config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-async function run() {
-
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-  const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [{ text: "Hello, I have 2 dogs in my house." }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Great to meet you. What would you like to know?" }],
-      },
-    ],
-    generationConfig: {
-      maxOutputTokens: 100,
-    },
-  });
-
-  const msg = "How many paws are in my house?";
-
-  const result = await chat.sendMessage(msg);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
-
-run();
 
 
-
-/*const client = new Client({
+const client = new Client({
     webVersion: "2.2409.4-beta",
     webVersionCache: {
       type: "remote",
@@ -53,27 +23,60 @@ client.on('qr',(qr)=>{
 });
 
 client.on('message_create', async (message) => {
+  const prompt = message.body;
     // Ignore messages from the bot itself
     if (message.fromMe) return;
   
     try {
       
-  
+      let chat = await message.getChat();
       // Check if the message is from a personal chat
       if (!chat.isGroup) {
-        const prompt = message.body;
+        
   
         // Check if the message body is 'Hey' or 'hey'
-        if (message.body.toLowerCase() === 'hey') {
+        if (prompt.toLowerCase() === 'hey') {
           await message.reply('hey, aksa here!!');
         } else {
-
+          await run(message, prompt);
         }
       }
     } catch (error) {
       console.error('Error with OpenAI API:', error);
-      await message.reply('Sorry, there was an error processing your request.');
+      await message.reply('I will get back with you asap');
     }
   });
   
-  client.initialize();*/
+ 
+
+  async function run(message, prompt) {
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+  
+    const chat = model.startChat({
+      history: [
+        {
+          role: "user",
+          parts: [{ text: "Hello, I have 2 dogs in my house." }],
+        },
+        {
+          role: "model",
+          parts: [{ text: "Great to meet you. What would you like to know?" }],
+        },
+      ],
+      generationConfig: {
+        maxOutputTokens: 100,
+      },
+    });
+
+  
+    const result = await chat.sendMessage(prompt);
+    const response = await result.response;
+    const text = await response.text();
+    await message.reply(text);
+  }
+ 
+  client.initialize();
+ 
+  
+  
